4a786ae3b6da2ef8dad8b7daa267d24821f529db
Articool12 <58137347+Articool12@users.noreply.github.com>
2019-11-24 19:16:51 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ТроелсенГлава2консоль
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Моё крутое приложение номер 1";
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.WriteLine("----------------------------");
+            Console.WriteLine("-----Моя прога номер 1------");
+            Console.WriteLine("----------------------------");
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ReadLine();
+        }
+    }
+}
